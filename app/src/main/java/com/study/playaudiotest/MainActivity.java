d9e24e4a92c6a9918790cbee7dd1ddc26685066d
WXB506 <1922686887@qq.com>
2016-03-08 22:52:25 +0800

    play audio test

---
+++ app/src/main/java/com/study/playaudiotest/MainActivity.java
@@ -0,0 +1,86 @@
+package com.study.playaudiotest;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Environment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class MainActivity extends Activity implements AdapterView.OnClickListener{
+
+    private Button play;
+    private Button pause;
+    private Button stop;
+
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        play = (Button) findViewById(R.id.play);
+        play.setOnClickListener(this);
+
+        pause = (Button)findViewById(R.id.pause);
+        pause.setOnClickListener(this);
+
+        stop = (Button) findViewById(R.id.stop);
+        stop.setOnClickListener(this);
+
+        initMediaPlayer();
+    }
+
+    private void initMediaPlayer() {
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(), "music.mp3");
+            Toast.makeText(this, file.getPath().toString(), Toast.LENGTH_LONG).show();
+
+            mediaPlayer.setDataSource(file.getPath().toString());
+            mediaPlayer.prepare();
+        } catch (Exception e) {
+            //e.printStackTrace();
+            Log.d("Fuck", "Fuck");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.play:
+                if(!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();
+                }
+                break;
+            case R.id.pause:
+                if(mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                }
+                break;
+            case R.id.stop:
+                if(mediaPlayer.isPlaying()) {
+                    mediaPlayer.reset();
+                    initMediaPlayer();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+    }
+}
